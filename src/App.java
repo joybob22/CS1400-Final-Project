82ff3946f326d84f4e610bfe7654974a1d025143
joybob22 <braydenlemke8@gmail.com>
2021-04-06 11:16:34 -0600

    created the typed output code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!!");
+        Output.type("Look at me type this output to the screen!!");
     }
 }
