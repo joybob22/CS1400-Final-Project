d96111376f01118318e1d0ac9c813886f7ebcbf4
Ike <Ike@Michaels-MacBook-Air.local>
2021-04-15 10:06:20 -0600

    isaac commit

--- src/StoryMiddle.java
+++ src/StoryMiddle.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+
 public class StoryMiddle {
-
+   
    private static Scanner input = new Scanner(System.in);
+   
+       public static void StoryMiddle() {
+       Output.type("INCOMING TRANSMISSION", Colors.ANSI_GREEN);
+       System.out.println();
+       Output.delayWithDots(5);
+       Output.choices("Yes I can hear you!", "New phone who dis?");
 
 
 
+       
+       
+   }
 }
